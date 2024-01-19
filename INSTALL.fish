8fa6383fe0745075d3a2e4c606298b20e0c65b20
Conradin Laux <conradin.laux@gmail.com>
2024-01-19 20:41:13 +0100

    feat: completions

--- INSTALL.fish
+++ INSTALL.fish
@@ -1,7 +1,11 @@
 #!/usr/bin/env fish
 mkdir -p "$HOME/.config/fish/functions"
-cp ./pngtex.fish "$HOME/.config/fish/functions/pngtex.fish"
+mkdir -p "$HOME/.config/fish/completions"
 
-mkdir -p "$HOME/.local/share/"
+cp ./pngtex.fish "$HOME/.config/fish/functions/pngtex.fish"
+cp ./completions.fish "$HOME/.config/fish/completions/pngtex.fish"
+
+mkdir -p "$HOME/.local/share"
 cp ./hyde.sty "$HOME/.local/share/hyde.sty"
+
 
---
+++ completions.fish
@@ -0,0 +1,5 @@
+complete -c pngtex -s c -l cmd -d 'sets the command invoked to save the image.'
+complete -c pngtex -s s -l save -d 'saves the file to the specified location' -r
+complete -c pngtex -s h -l help -d 'shows help message'
+complete -c pngtex -s I -l replace -d 'replaces pattern with the output file when showing the image' -r
+
